09f5e43ae44234644cbf2d5878ee8b95323100b2
Marko Kruljac <marko.kruljac@degordian.com>
2016-02-26 16:39:53 +0100

    bootstrapped renderer

--- RouteBootstrap.php
+++ RouteBootstrap.php
@@ -18,7 +18,20 @@ class RouteBootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
+        $this->addFrontendUrlRule($app);
+        $this->setFrontendViewRenderer($app);
+    }
+
+    private function addFrontendUrlRule(Application $app)
+    {
         $rules = ['frontend/<layout:\w+><view:(.*)>' => 'frontend/frontend/render-raw',];
-        Yii::$app->getUrlManager()->addRules($rules);
+        $app->getUrlManager()->addRules($rules);
+    }
+
+    private function setFrontendViewRenderer(Application $app)
+    {
+        $app->view->renderers['php'] = [
+            'class' => 'degordian\frontendController\renderer\FrontendRenderer'
+        ];
     }
 }
